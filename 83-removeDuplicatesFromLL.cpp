0be156e27191cd34a31ffedafcfbd313b23cbc21
Ravinder <itskumarravinder@gmail.com>
2023-08-30 20:56:43 +0530

    Remove Duplicates from LL

---
+++ 83-removeDuplicatesFromLL.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* iterator = head;
+        while(iterator != nullptr && iterator->next != nullptr){
+            if(iterator->val == iterator->next->val) iterator->next = iterator->next->next;
+            else iterator = iterator->next;
+        }
+
+        return head;
+    }
+};
